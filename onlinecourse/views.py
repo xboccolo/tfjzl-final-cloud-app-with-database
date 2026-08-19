7e9f6aaa857afe97da4329cdeea8420ca43a46fc
xboccolo <edoboccolo@gmail.com>
2026-08-19 18:17:19 +0200

    show exam results with bootstrap updated

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -161,11 +161,12 @@ def show_exam_result(request, course_id, submission_id):
 
     context = {
         'course': course,
-        'total_score': total_score,
-        'choices': choices
+        'grade': total_score,
+        'choices': choices,
+        'questions': questions
     }
 
-    return render(request, 'onlinecourse/show_exam_result_bootstrap.html', context)
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
 
